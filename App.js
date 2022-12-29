ab3ab8b7c687ef1fbdd7ba6a74d1df64414978fe
Arthur Chaloin <arthur.chaloin@gmail.com>
2022-12-29 12:44:23 +0100

    react native bug demo

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ marginTop: 50 }}>
+      <Text style={{ fontSize: 32 }}>Hello, world</Text>
+      <TextInput
+        multiline
+        scrollEnabled={false}
+        style={{
+          lineHeight: 22,
+          backgroundColor: 'red',
+        }}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
